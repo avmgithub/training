9b89cfaa290cd06e68e506037df2c0b66a2eee06
Freddie Mendoza <mendoza1@us.ibm.com>
2018-10-01 09:50:01 -0400

    ppc64le changes

--- object_detection/caffe2/extract_dataset.sh
+++ object_detection/caffe2/extract_dataset.sh
@@ -1,15 +1,19 @@
-mkdir detectron/lib/datasets/data/coco
+mkdir -p detectron/lib/datasets/data/coco
 mv coco_annotations_minival.tgz detectron/lib/datasets/data/coco
 mv train2014.zip detectron/lib/datasets/data/coco
 mv val2014.zip detectron/lib/datasets/data/coco
 mv annotations_trainval2014.zip detectron/lib/datasets/data/coco
 
 cd detectron/lib/datasets/data/coco
-dtrx --one=here coco_annotations_minival.tgz
-dtrx --one=here annotations_trainval2014.zip
+tar -xzvf coco_annotations_minival.tgz
+#dtrx --one=here coco_annotations_minival.tgz
+unzip annotations_trainval2014.zip
+#dtrx --one=here annotations_trainval2014.zip
 mv annotations.1/* annotations/
 
-dtrx train2014.zip
+#dtrx train2014.zip
+unzip train2014.zip
 mv train2014/ coco_train2014/
-dtrx val2014.zip
+#dtrx val2014.zip
+unzip val2014.zip
 mv val2014/ coco_val2014/
